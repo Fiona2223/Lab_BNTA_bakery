67820abaa2a3166c1b503ff36ba9daf84e3b6cab
Your Name <you@example.com>
2023-05-17 16:19:35 +0100

    first commit

---
+++ start_code/src/cake/Cake.js
@@ -0,0 +1,15 @@
+const Cake = ({cakeName, ingredients, price, rating}) => {
+
+  const showList= (listOfIngredients) => {
+   
+  }
+
+  return (
+    <>
+        <h2>This is a cake</h2>
+      
+    </>
+  )
+}
+
+export default Cake;
